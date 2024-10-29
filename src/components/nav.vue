69c7bcddee02928a616b9c4fdcaf3251da657fc8
Nathan Faucher <nathan.j.faucher@gmail.com>
2024-10-29 15:46:32 -0600

    Added underline under navbar when scrolling
    If page content extends past the screen size when the page is scrolled the navbar will have an underline under it to differentiate between the page content and the navbar content

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -3,14 +3,18 @@ import Sidebar from "@/components/sidebar.vue";
 import Navbar from "@/components/navbar.vue";
 </script>
 <template>
-  <div class="fixed inset-y-0 z-50 flex flex-col w-60">
+  <div class="fixed inset-y-0 z-50 flex flex-col w-60 bg-base-100">
     <Sidebar />
   </div>
   <div class="fixed inset-0 grow flex flex-col overflow-y-hidden pl-60">
-    <div class="top-0 h-16">
+    <!-- Uncomment below for a shadow instead of a line -->
+    <!--<div class="top-0 h-16 shadow shadow-base-content-200">-->
+    <div class="top-0 h-16 ring-1 ring-base-content-200">
       <Navbar />
     </div>
     <main class="overflow-y-auto h-full">
+      <div class="-top-2 sticky bg-base-100 h-2"></div>
+      <div class="top-0 sticky h-0.5"></div>
       <slot />
     </main>
   </div>
